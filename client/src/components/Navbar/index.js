b1e9d377367492dd8262a507dc40def282e1d53a
tho.mai <tho.mai@lotussoft.vn>
2021-10-22 22:55:43 +0700

    update again connect meta mask

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,40 +1,45 @@
 import React, {useState, useEffect} from "react";
 import Web3 from 'web3';
 
+let web3;
+
 const Navbar = ({onSwitch, showAsk}) => {
 
   const [account, setAccount] = useState("Connect to wallet");
   const [metamask, setMetamask] = useState(true);
 
-  useEffect(() => {
-    loadWeb3();
-    loadData();
-    if (window.localStorage.account != undefined && window.web3 != undefined)
-      setAccount(window.localStorage.account)
-  }, [account]);
-  
+  useEffect(async() => {
+      const ethereum = window.ethereum;
+      if (ethereum != undefined){
+        ethereum.on('accountsChanged', function(accounts){
+          setAccount(accounts[0]);
+          window.localStorage.account=accounts[0];
+          if (window.localStorage.account == 'undefined') setAccount("Connect to wallet")
+        })
+        
+      }else{
+        setAccount("Connect to wallet")
+      }
+      if (window.localStorage.account != 'undefined' && typeof window.localStorage.account != 'undefined'){
+        setAccount(window.localStorage.account)
+      }
+      else setAccount("Connect to wallet")
+  }, []);
+
   const loadWeb3 = async ()=>{
-    if (window.ethereum){
-      window.web3 = new Web3(window.ethereum);
-      await window.eth_requestAccounts;
+    if (window.ethereum != undefined){
+      web3 = new Web3(window.ethereum);
+      await window.ethereum.request({ method: 'eth_requestAccounts' }).then(result => {
+        setAccount(result[0]);
+        console.log(result[0]);
+        window.localStorage.account=result[0];
+    })
       setMetamask(true);
     }else if(window.web3){
-      window.web3 = new Web3(window.web3.currentProvider);
+      web3 = new Web3(window.web3.currentProvider);
       setMetamask(true);
     }else{
       setMetamask(false);
-    }
-  }
-
-  const loadData = async ()=>{
-    if (window.web3){
-      const web3 = window.web3;
-      const a = await web3.eth.getAccounts((error, result) =>{
-        if (result.length != 0 && result[0] !=undefined){
-          setAccount(result);
-          window.localStorage.account = result;
-        }
-    });
     }
   }
 
@@ -49,7 +54,7 @@ const Navbar = ({onSwitch, showAsk}) => {
         <label for="bidSlider" class="bidLabel slide" onClick={onSwitch}>BID</label>
         <div class="slider-tab"></div>
       </div>
-      <button className="connectWallet" onClick={loadData}>{account}</button>
+      <button className="connectWallet" onClick={loadWeb3}>{account}</button>
     </nav>
     {!metamask&&(
         <div style={{backgroundColor: "green", position: "absolute", left: "35%", zIndex: "999"}}>
