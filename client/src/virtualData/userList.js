b5e9fcf0078f6852b8c30c5d82b8817db219eedf
Phan Hoàng Dung <18520027@gm.uit.edu.vn>
2021-10-31 22:34:38 +0700

    create profile page

---
+++ client/src/virtualData/userList.js
@@ -0,0 +1,28 @@
+export const userList = {
+    users: [
+        {
+            id: '1',
+            name: 'phd',
+            key: '0x02982a4e785690157926c46c4581e819325e6310',
+            joinedDate: 'September 2021'
+        },
+        {
+            id: '2',
+            name: 'ttyl',
+            key: '0x02982a4e785690157926c46c4581e819325e6302',
+            joinedDate: 'May 2021'
+        },
+        {
+            id: '3',
+            name: 'mndt',
+            key: '0x02982a4e785690157926c46c4581e819325e6302',
+            joinedDate: 'June 2021'
+        },
+        {
+            id: '4',
+            name: 'npl',
+            key: '0x02982a4e785690157926c46c4581e209325e6302',
+            joinedDate: 'July 2021'
+        }
+    ]
+}
