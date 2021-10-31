2a851c61fc292d9459c6e5653d51ff277bd4044b
Lan Truong <truongylan2000@gmail.com>
2021-10-31 12:06:46 +0700

    Restyle product list page

--- client/src/containers/ProductList/index.js
+++ client/src/containers/ProductList/index.js
@@ -8,12 +8,12 @@ const ProductList = () => {
     return (
        <>
             <div>
-                <div className = "row">
-                    <div className = "col-3">
+                <div className = "row gx-4 m-0 pl-3 pr-3">
+                    <div className = "col-3 pr-5 pl-0">
                         <Filter></Filter>
                     </div>
-                    <div className = "col-9">
-                        <div className="row justify-content-between">
+                    <div className = "col-9 p-0">
+                        <div className="row justify-content-between m-0">
                             {
                                 productList.products.map((product, index) => (
                                     <Product 
