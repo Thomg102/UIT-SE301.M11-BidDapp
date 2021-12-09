dc96ead817918fc1a45aceadcc902968f2a86920
Lan Truong <truongylan2000@gmail.com>
2021-12-09 08:09:12 +0700

    Add favicon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/static', express.static(path.join(__dirname, '/build/static')))
 app.get('*', function (req, res) {
